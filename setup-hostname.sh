a2eb8d80c3125591bd4488c5a861af1ca508116c
Mike Vanis <hello@mikevanis.com>
2018-10-16 12:23:46 +0100

    added hostname change script

---
+++ setup-hostname.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+cd ~ &&
+echo -e "${BLUE}Changing hostname to visioncam...${NC}" &&
+sudo sed -i "s/raspberrypi.*/visioncam/" /etc/hostname &&
+sudo sed -i "s/raspberrypi.*/127.0.1.1  visioncam/" /etc/hosts &&
+
+echo -e "${GREEN}Done! Please restart manually to confirm that the hostname has changed.${NC}"
